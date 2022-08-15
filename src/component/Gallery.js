0e7807b8f3c9a0b27104ed675ec4b53b283f2f89
kaytee <https://github.com/akglitch/kayteeestate.git>
2022-08-15 04:07:45 +0000

    initial commit

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -10,9 +10,10 @@ function Gallery() {
         <div className="">
             <div className=" col-md g-0">
                 <div className="row g-0">
-                {gallery.map((item,id) => {
+                {gallery.map((item) => {
                     return (
-                      <div key={item.id == id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12" >
+                      <div key={item.id}>
+                      <div  className="col-lg-3 col-md-4 col-sm-6 col-xs-12" >
                       <div  class="hovereffect">
 
                       <img  src={item.img} height="250" alt="gallery"loading="" />
@@ -24,7 +25,7 @@ function Gallery() {
                       </p>
                       
                           </div>
-                
+                </div>
                          
                   </div>
                   </div>
