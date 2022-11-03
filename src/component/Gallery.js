8f1e48765d8f7abc2b0cda7aceaefd1aa5fc879d
Aninakwa kwaku ntiri <91568087+akglitch@users.noreply.github.com>
2022-11-03 04:38:03 -0400

    Update Gallery.js

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -19,7 +19,7 @@ function Gallery() {
                       <div key={item.id} className="col-lg-4 col-md-4 col-sm-6 col-xs-12" >
                       <div  class="hovereffect">
 
-                      <img src={item.img} height="300" width="250" alt="gallery" />
+                      <img src={item.img} height="300" width="300" alt="gallery" />
                     
                     <div class="overlay">
                               <h2>{item.name}</h2>
