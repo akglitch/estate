1786e763f529ac4e60dba9236b7ad98af9c3be71
kaytee <mcaninakwa@gmail.com>
2022-11-14 06:45:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import Footer from './component/Footer';
 import Gallery from './component/Gallery';
+import Welcome from './component/Welcome';
 
 
 function App() {
   return (
     <div >
+      <Welcome />
      <Gallery />
      <Footer />
     </div>
---
+++ src/component/Welcome.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Welcome () {
+    return (
+        <div className="opening">
+
+        </div>
+    )
+}
