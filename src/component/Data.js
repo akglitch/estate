c4613ceb7c3b3d96bafda9fe893854ed321f789b
kaytee <mcaninakwa@gmail.com>
2022-11-14 06:22:52 -0500

    initial commit

--- src/component/Data.js
+++ src/component/Data.js
@@ -2,63 +2,63 @@
     {
         "id":1,
         "name":"sofa",
-        "img":require("../Images/decor.jpg"),
+        "img":require("../Images/gallery10.jpg"),
         "description":"hello world welcome come to kumasi",
     },
     {
         "id":2,
         "name":"chair",
-        "img": require("../Images/decor2.jpg")
+        "img": require("../Images/gallery2.jpg")
     },
     {
         "id":3,
         "name":"wardrobe",
-        "img": require("../Images/decor3.jpg")
+        "img": require("../Images/gallery9.jpg")
     },
     {
         "id":4,
         "name":"sofa",
-        "img":require("../Images/decor4.jpg"),
+        "img":require("../Images/gallery5.jpg"),
     },
     {
         "id":5,
         "name":"sofa",
-        "img": require("../Images/decor5.jpg")
+        "img": require("../Images/gallery7.jpg")
     },
     {
         "id":6,
         "name":"sofa",
-        "img": require("../Images/decor6.jpg")
+        "img": require("../Images/gallery1.jpg")
     },
     {
         "id":7,
         "name":"sofa",
-        "img": require("../Images/decor7.jpg")
+        "img": require("../Images/gallery.jpg")
     },
     {
         "id":8,
         "name":"sofa",
-        "img": require("../Images/decor8.jpg")
+        "img": require("../Images/gallery3.jpg")
     },
     {
         "id":9,
         "name":"sofa",
-        "img": require("../Images/decor9.jpg")
+        "img": require("../Images/gallery4.jpg")
     },
     {
         "id":10,
         "name":"sofa",
-        "img": require("../Images/decora.jpg")
+        "img": require("../Images/gallery11.jpg")
     },
     {
         "id":11,
         "name":"sofa",
-        "img": require("../Images/decorb.jpg")
+        "img": require("../Images/gallery12.jpg")
     }, 
     {
         "id":12,
         "name":"sofa",
-        "img": require("../Images/decorg.jpg")
+        "img": require("../Images/gallery13.jpg")
     },
    
     
