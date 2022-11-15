32c3e02d110b9a7fbee0ec28e0f55be4eb821088
kaytee <mcaninakwa@gmail.com>
2022-11-15 06:17:31 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import Background from './Components/Background';
 
-import Footer from './component/Footer';
-import Gallery from './component/Gallery';
-import Welcome from './component/Welcome';
+import Footer from './Components/Footer';
+import Gallery from './Components/Gallery';
+import Nav from './Components/Nav';
+
 
 
 function App() {
   return (
     <div >
-      <Welcome />
+   <Nav />
+   <Background />
      <Gallery />
      <Footer />
     </div>
---
+++ src/Components/Background.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+export default function Background () {
+    return(
+        <div  className="opening">
+
+        </div>
+    )
+}
