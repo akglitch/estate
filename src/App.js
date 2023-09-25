d2f604b30e43f07f2076bacde061211ee48927d4
kaytee <https://github.com/akglitch/kayteeestate.git>
2023-09-25 10:15:40 +0000

    simple tweaks

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Footer from './Components/Footer';
-import { Hero } from './Components/Hero';
+
 import Nav from './Components/Nav';
-import Herotwo from './Components/Herotwo';
-import Herothree from './Components/Herothree';
-import Team from './Components/Team';
-import Testimonials from './Components/Testimonials';
-import Gallery from './Components/Gallery'; // Import Gallery component
+
 import About from './Components/About';
 import Listings from './Components/Listings';
 import Contact from './Components/Contact';
+import Home from './Components/Home';
 
 
 function App() {
@@ -23,7 +20,7 @@ function App() {
         <Nav />
         
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/listings" element={<Listings />} />
           <Route path="/contact" element={<Contact />} />
@@ -35,18 +32,6 @@ function App() {
   );
 }
 
-function Home() {
-  return (
-    <>
-    <Hero />
-      <Herotwo />
-      <Herothree />
-      <Team />
-      <Gallery />
-      <Testimonials />
 
-    </>
-  );
-}
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Hero } from './Hero';
+import Herotwo from './Herotwo';
+import Herothree from './Herothree';
+import Team from './Team';
+import Testimonials from './Testimonials';
+import Gallery from './Gallery'; // Import Gallery component
+
+  function Home() {
+    return ( 
+        <div>
+<Hero />
+        <Herotwo />
+       <Herothree />
+        <Team />
+        <Gallery />
+        <Testimonials />
+        </div>
+     );
+  }
+  
+  export default Home;
