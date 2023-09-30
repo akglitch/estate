6531e8d554eb6268b8183fca7ca0157fb0aadc16
kaytee <https://github.com/akglitch/kayteeestate.git>
2023-09-30 21:40:46 +0000

    simple tweaks

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -23,7 +23,8 @@ export default function App() {
       
 <div className=' background-text container-fluid mt-2 mb-5'>
           <h1 class=" font-sans text-2xl mt-7  ">Welcome to Your Dream Home</h1>
-          <p>At AKproperties, we understand that finding the perfect home is more than just a transaction; it's about finding a place where memories are created, <br></br>families grow, and dreams come to life. With our expertise and commitment to excellence, we're here to help you turn your real estate dreams into reality</p>
+          <p>
+At AKproperties, we understand that a home is where memories are born and dreams take flight. With our expertise and commitment, we make your real estate dreams a reality.</p>
           <a href="#y" class="btn btn-outline-primary">Learn more</a>
           <p className="text-center"></p>
           </div>
